b24e0d71d348ff9cf79b5752b2f2daf718b7cee0
Felipe Menino <efelipecarlos@gmail.com>
2022-04-23 11:22:13 -0300

    tests: configuring package to tests

--- setup.py
+++ setup.py
@@ -14,28 +14,57 @@ from setuptools import find_packages, setup
 readme = open("README.rst").read()
 history = open("CHANGES.rst").read()
 
+# Should follow inveniosoftware/invenio versions
+invenio_db_version = ">=1.0.11,<2.0.0"
+invenio_search_version = ">=1.4.0,<2.0.0"
+
 tests_require = [
+    "pytest-mock>=1.6.0",
     "pytest-invenio>=1.4.0",
+    "invenio-app>=1.3.1,<2.0.0",
+    "invenio-db[postgresql,versioning]{}".format(invenio_db_version),
 ]
 
 extras_require = {
-    "docs": [
-        "Sphinx>=3,<4",
+    "docs": ["sphinx>=4.2.0,<5"],
+    # Elasticsearch version
+    "elasticsearch7": [
+        "invenio-search[elasticsearch7]{}".format(invenio_search_version),
+    ],
+    # Databases
+    "mysql": [
+        "invenio-db[mysql,versioning]{}".format(invenio_db_version),
+    ],
+    "postgresql": [
+        "invenio-db[postgresql,versioning]{}".format(invenio_db_version),
+    ],
+    "sqlite": [
+        "invenio-db[versioning]{}".format(invenio_db_version),
     ],
     "tests": tests_require,
 }
 
 extras_require["all"] = []
-for reqs in extras_require.values():
+for name, reqs in extras_require.items():
+    if name[0] == ":" or name in (
+        "elasticsearch7",
+        "mysql",
+        "postgresql",
+        "sqlite",
+        "docs",
+    ):
+        continue
     extras_require["all"].extend(reqs)
+
 
 setup_requires = [
     "Babel>=2.8",
+    "pytest-runner>=3.0.0,<5",
 ]
 
 install_requires = [
     "invenio-i18n>=1.2.0",
-    "invenio-rdm-records>=0.32.6",
+    "invenio-rdm-records @ git+https://github.com/geo-knowledge-hub/invenio-rdm-records@b-1.0",
 ]
 packages = find_packages()
 
@@ -51,7 +80,7 @@ setup(
     version=version,
     description=__doc__,
     long_description=readme + "\n\n" + history,
-    keywords="invenio TODO",
+    keywords=["GEO Knowledge Hub", "Invenio Module", "Configuration"],
     license="MIT",
     author="GEO Secretariat",
     author_email="geokhub@geosec.org",
--- tests/test_geo_config.py
+++ tests/test_geo_config.py
@@ -29,10 +29,3 @@ def test_init():
     assert 'geo-config' not in app.extensions
     ext.init_app(app)
     assert 'geo-config' in app.extensions
-
-
-def test_view(base_client):
-    """Test view."""
-    res = base_client.get("/")
-    assert res.status_code == 200
-    assert 'Welcome to geo-config' in str(res.data)
